4c8ec6a8ff46862bd8155c87f777f9b9f86c1a1c
JozPedro23zx <josepedro23zx@gmail.com>
2024-06-16 10:21:22 -0300

    add find all student usecase

---
+++ src/core/student/application/find-student/find-all-students.usecase.ts
@@ -0,0 +1,31 @@
+import { IUseCase } from "@core/@shared/application/use-case-interface";
+import { StudentOutput, StudentOutputMapper } from "../output/student-output";
+import { IStudentRepository } from "@core/student/infrastructure/student-interface.repository";
+import { Uuid } from "@core/@shared/domain/value-object/uuid.vo";
+import { Student } from "@core/student/domain/student.entity";
+
+export class FindAllStudentUsecase implements IUseCase<FindAllStudentUsecaseInput, StudentOutput[]>{
+    constructor(private readonly studentRepo: IStudentRepository){}
+
+    async execute(input: FindAllStudentUsecaseInput): Promise<StudentOutput[]> {
+        let students: Student[];
+        if(!input.ids || input.ids.length === 0){
+            students = await this.studentRepo.findAll();
+        }else if(input.ids.length > 0){
+            let uuids = input.ids.map((id) => new Uuid(id));
+            students = await this.studentRepo.findByIds(uuids);
+        }
+
+        let output: StudentOutput[];
+        for (let student of students){
+            let out = StudentOutputMapper.toOutput(student);
+            output.push(out);
+        }
+
+        return output;
+    }
+}
+
+type FindAllStudentUsecaseInput = {
+    ids?: string[]
+}
