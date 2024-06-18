41769afeffcb0a6407e58bf87972bef0dcd0467b
JozPedro23zx <josepedro23zx@gmail.com>
2024-06-18 17:22:33 -0300

    add config module

---
+++ src/modules/config/config.module.ts
@@ -0,0 +1,56 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+import { join } from 'path';
+
+type DB_SCHEMA_TYPE = {
+    DB_VENDOR: 'mysql' | 'sqlite';
+    DB_HOST: string;
+    DB_PORT: number;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+    DB_DATABASE: string;
+    DB_LOGGING: boolean;
+    DB_AUTO_LOAD_MODELS: boolean;
+};
+
+export type CONFIG_SCHEMA_TYPE = DB_SCHEMA_TYPE;
+
+export const CONFIG_DB_SCHEMA: Joi.StrictSchemaMap<DB_SCHEMA_TYPE> = {
+    DB_VENDOR: Joi.string().required().valid('postgres', 'sqlite'),
+    DB_HOST: Joi.string().required(),
+    DB_DATABASE: Joi.string().when('DB_VENDOR', {
+      is: 'postgres',
+      then: Joi.required(),
+    }),
+    DB_USERNAME: Joi.string().when('DB_VENDOR', {
+      is: 'postgres',
+      then: Joi.required(),
+    }),
+    DB_PASSWORD: Joi.string().when('DB_VENDOR', {
+      is: 'postgres',
+      then: Joi.required(),
+    }),
+    DB_PORT: Joi.number().integer().when('DB_VENDOR', {
+      is: 'postgres',
+      then: Joi.required(),
+    }),
+    DB_LOGGING: Joi.boolean().required(),
+    DB_AUTO_LOAD_MODELS: Joi.boolean().required(),
+  };
+
+@Module({})
+export class MyConfigModule extends ConfigModule{
+    static forRoot(){
+
+        const envFilePath = process.env.NODE_ENV 
+        ? join(process.cwd(), 'envs', `.env.${process.env.NODE_ENV}`)
+        : join(process.cwd(), 'envs', '.env');
+
+        return super.forRoot({
+            isGlobal: true,
+            envFilePath: [envFilePath, '.env'],
+            validationSchema: Joi.object({...CONFIG_DB_SCHEMA})
+        })
+    }
+}
